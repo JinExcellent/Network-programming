2dfb907887a623b0b2baf701db60579db6ee4f0e
JinExcellent <jinexcellentone@gmail.com>
2025-03-30 18:33:15 +0800

    This is a C/S model based on thread implementations

--- proc_CS/thread_serve.c
+++ proc_CS/thread_serve.c
@@ -9,11 +9,17 @@
 #include <pthread.h>
 
 
+#define BUF_SIZE 100
+#define MAX_CLIN 256
 
+
+void *handle_clin(void * arg);
+void send_mas(char * msg, int len);
 
 
 pthread_mutex_t mutx;
-
+int clin_count = 0;
+int clin_socks[MAX_CLIN];
 
 int main(int argc, char *argv[]){
     struct sockaddr_in serv_addr, clin_addr;
@@ -37,19 +43,59 @@ int main(int argc, char *argv[]){
     }
     listen(sock_l, 5);
 
+    pthread_mutex_init(&mutx, NULL);
+    pthread_t t_id;
+
     while (1) {
         clin_addr_sz = sizeof(clin_addr);
         sock_c = accept(sock_l, (struct sockaddr *)&clin_addr, &clin_addr_sz);
+
+        pthread_mutex_lock(&mutx);
+        clin_socks[clin_count++] = sock_c;
+        pthread_mutex_unlock(&mutx);
+
+        pthread_create(&t_id, NULL, handle_clin, (void*)&sock_c);
+        pthread_detach(t_id);
+        printf("connected client IP: %s and port: %d\n", inet_ntoa(clin_addr.sin_addr), clin_addr.sin_port);
                     
-    }
-
-
+    }  
+    close(sock_l);
     return 0;
 }
 
 
+void *handle_clin(void * arg){
+    int clin_sock = *((int*)arg);
+    int str_len = 0;
+    char msg[BUF_SIZE];
+
+    while((str_len = read(clin_sock, msg, sizeof(msg))) != 0)
+        send_mas(msg, str_len);
+
+    pthread_mutex_lock(&mutx);
+    for(int i = 0; i < clin_count; i++){
+        if(clin_sock == clin_socks[i]){
+            while (i < clin_count - 1) {    //这里使用数组内部元素移动的方式处理需要断开的套接字
+                clin_socks[i] = clin_socks[i + 1];
+                i++;
+            }
+            break;
+        }
+    }
+    clin_count--;
+    pthread_mutex_unlock(&mutx);
+    close(clin_sock);
+    return NULL;
+}
 
 
+void send_mas(char * msg, int len){
+    pthread_mutex_lock(&mutx);
+    for(int i = 0; i < clin_count; i++){
+        write(clin_socks[i], msg, len);
+    }
+    pthread_mutex_unlock(&mutx);
+}
 
 
 
